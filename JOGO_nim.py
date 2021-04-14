50a5c4e33e6ff3681fb2cd7ea3935f91c8fad8b7
DaniloNunesBR <45714647+DaniloNunesBR@users.noreply.github.com>
2021-04-14 13:06:30 -0300

    Update JOGO_nim.py

--- JOGO_nim.py
+++ JOGO_nim.py
@@ -1,14 +1,3 @@
-"""
-Este Jogo envolve Matemática ! 
-
-É simples de jogar ! 
-1. Você escolhe um número de peças total para o tabuleiro.
-2. Você escolhe o limite de peças pra tirar por partida.
-3. As partidas se revezam entre você e o computador.
-4. O ganhador é aquele que conseguir tirar as últimas peças !
-
-Obs: O computador sempre ganha !
-"""
 def computador_escolhe_jogada(n,m):
     m = m + 1
     multiplo = n % m
